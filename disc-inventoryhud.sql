c5b31a236061e61353c3e7d843a53c347c8f5f26
DiscworldZA <pd.van.lill@gmail.com>
2019-12-17 19:21:32 +0200

    sql

---
+++ disc-inventoryhud.sql
@@ -0,0 +1,10 @@
+﻿create table if not exists esx_essentialmode.disc_inventory
+(
+	id int auto_increment
+		primary key,
+	owner text not null,
+	type text null,
+	slot int not null,
+	data longtext not null
+);
+
