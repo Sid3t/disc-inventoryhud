58d7e4654276637d4858cb828ce888ec8015cb85
DiscworldZA <pd.van.lill@gmail.com>
2019-12-19 23:45:09 +0200

    sql

--- disc-inventoryhud.sql
+++ disc-inventoryhud.sql
@@ -1,10 +1,27 @@
-﻿create table if not exists esx_essentialmode.disc_inventory
+﻿create table esx_essentialmode.disc_inventory
 (
 	id int auto_increment
 		primary key,
-	owner text not null,
-	type text null,
+	owner varchar(50) not null,
+	type varchar(10) null,
 	slot int not null,
 	data longtext not null
+);
+
+create index disc_inventory_owner_type_index
+	on esx_essentialmode.disc_inventory (owner, type);
+
+create table esx_essentialmode.disc_inventory_itemdata
+(
+	id bigint unsigned auto_increment,
+	item varchar(50) not null,
+	description text not null,
+	label varchar(50) not null,
+	meta tinyint(1) default 0 not null,
+	itemurl text not null,
+	constraint disc_inventory_itemdata_item_uindex
+		unique (item),
+	constraint id
+		unique (id)
 );
 
