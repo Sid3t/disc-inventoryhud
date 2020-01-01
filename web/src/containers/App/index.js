8416d2afb7207eba918bd76434e7020d00e1de09
DiscworldZA <pd.van.lill@gmail.com>
2020-01-01 15:40:53 +0200

    add esc and f2 to close inv since rick and newb and alzars bitches and moans

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { makeStyles, MuiThemeProvider } from '@material-ui/core';
 import Theme from '../../../theme/Theme';
 import Screen from '../../components/UI/AppScreen/AppScreen';
+import { hideApp } from './actions';
+import Nui from '../../util/Nui';
 
 const useStyles = makeStyles(theme => ({}));
 
 
-const App = ({ hidden }) => {
+const App = (props) => {
   const classes = useStyles();
-
+  const closeFunction = (event) => {
+    if(event.which === 27 || event.which === 113) {
+      props.dispatch(hideApp);
+      Nui.send('CloseUI');
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('keydown', closeFunction);
+    return () => window.removeEventListener('keydown', closeFunction);
+  }, []);
 
   return (
     <MuiThemeProvider theme={Theme}>
-      <Screen hidden={hidden} />
+      <Screen hidden={props.hidden} />
     </MuiThemeProvider>
 
   );
